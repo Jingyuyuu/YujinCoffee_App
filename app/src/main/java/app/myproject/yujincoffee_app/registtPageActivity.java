a5fa3184310f91f1903611bc97ebbfdee8465de8
jingyu <rose622612@gmail.com>
2023-01-15 15:40:15 +0800

    更新註冊頁面UI

--- app/src/main/java/app/myproject/yujincoffee_app/registtPageActivity.java
+++ app/src/main/java/app/myproject/yujincoffee_app/registtPageActivity.java
@@ -3,12 +3,23 @@ package app.myproject.yujincoffee_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import app.myproject.yujincoffee_app.databinding.ActivityRegisttPageBinding;
 
 public class registtPageActivity extends AppCompatActivity {
-
+    ActivityRegisttPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registt_page);
+        binding=ActivityRegisttPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.backLogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
