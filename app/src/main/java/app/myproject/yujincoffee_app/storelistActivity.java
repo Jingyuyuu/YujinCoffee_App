f5a4a261695a53bb4d18d79a9670e71c2f0d88b3
catherine9504 <catherine9504@hotmail.com.tw>
2023-01-16 16:02:25 +0800

    1/16 16:02

---
+++ app/src/main/java/app/myproject/yujincoffee_app/storelistActivity.java
@@ -0,0 +1,15 @@
+package app.myproject.yujincoffee_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class storelistActivity extends AppCompatActivity {
+
+    storelistActivity binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storelist);
+    }
+}
