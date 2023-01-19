a75341e15fc0714f7fe4d6cdc7ed067f1ff6f92b
Jingyuyuu <rose622612@gmail.com>
2023-01-19 17:41:36 +0800

    01/19-16:02 JSONArray

--- app/src/main/java/app/myproject/yujincoffee_app/MenuTestActivity.java
+++ app/src/main/java/app/myproject/yujincoffee_app/MenuTestActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MenuTestActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +23,31 @@ public class MenuTestActivity extends AppCompatActivity {
                 +" "+a.drinkItems.get(0).getIce()
                 +" "+a.drinkItems.get(0).getSugar()
                 +" "+a.drinkItems.get(0).getPrice());
+
+        JSONObject packet=new JSONObject();
+        JSONObject OrderMst=new JSONObject();
+        JSONArray OrderDetail=new JSONArray();
+        JSONObject drink001=new JSONObject();
+        JSONObject drink002=new JSONObject();
+        try {
+            packet.put("OrderMst",OrderMst);
+            OrderMst.put("member","黃曉明");
+            OrderMst.put("date","20230119");
+            drink001.put("飲料id","A001");
+            drink001.put("冷熱","冷");
+            drink001.put("數量","2");
+            Log.e("JSON",drink001.toString());
+            OrderDetail.put(drink001);
+            drink002.put("飲料id","A002");
+            drink002.put("冷熱","冷");
+            drink002.put("數量","4");
+            OrderDetail.put(drink002);
+            OrderMst.put("OrderDetail",OrderDetail);
+            Log.e("JSON",packet.toString());
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
